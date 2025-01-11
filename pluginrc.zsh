1bc169e5871caf0158228fda2d67f9577444d11e
Julian Prasetyo <jul14n4v@gmail.com>
2025-01-11 09:12:37 +0700

    Make Folder Completions

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -5,6 +5,7 @@ path=("$HOME/.local/bin" "$HOME/.bin" "$HOME/.config/composer/vendor/bin" "$HOME
 export PATH
 export ZIM_HOME="$CURR_DIR/zimfw"
 export ZSH_CACHE_DIR="$HOME/.zfunc"
+mkdir -p "$HOME/.zfunc/completions"
 
 if [ ! -x "$(command -v git)" ]; then
   zstyle ':zim:zmodule' use 'degit'
